1199c24fceeb61d03efc5935e11a4c187da20e48
prishupatel <prishupatel218@gmail.com>
2021-10-06 14:50:18 -0400

    this is my second version

--- src/day2/NestedIfElse.java
+++ src/day2/NestedIfElse.java
@@ -2,14 +2,14 @@ package day2;
 
 public class NestedIfElse {
     public static void main(String[] args) {
-        int day = 5;
+        int day = 2;
 
         if(day == 1)
         {
             System.out.println("Sunday");
         }
         else if (day==2){
-            System.out.println("Monday");
+            System.out.println("Monday is the day");
         }
         else if (day==3){
             System.out.println("Tuesday");
